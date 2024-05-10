fa9ec0a8cd700dac90e6eef20aeb52a4347a78f0
DDloveQQ <1207804129@qq.com>
2024-05-10 22:14:39 +0800

    第十章复习

---
+++ Chapter10/src/ThreadMethod/MillisCount.java
@@ -0,0 +1,15 @@
+package ThreadMethod;
+
+public class MillisCount {
+    public static void main(String[] args) {
+        for (int i = 10; i>=0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("新年快乐！");
+    }
+}
